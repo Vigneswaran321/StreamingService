f9fd49c9ad1e87a8dd49fb39998bed4071410b00
Vigneswaran321 <smartben0431@gmail.com>
2024-03-02 10:10:01 +0530

    mews

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -12,7 +12,7 @@ const sampleHeadlines = [
   "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 ];
 
-export default function news() {
+export default function News() {
   const [activeTab, setActiveTab] = useState("breaking");
 
   return (
